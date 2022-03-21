976a7b815459aa1428bca046a4e18adf6c4ac81d
alanchen-1 <alan_chen1@brown.edu>
2022-03-20 22:18:01 -0400

    add discriminator architecture and outline for unet gen

--- models/blocks.py
+++ models/blocks.py
@@ -1,3 +1,4 @@
+from msilib.schema import Patch
 import torch
 import torch.nn as nn
 
@@ -62,7 +63,6 @@ class ResNetBlock(nn.Module):
 
         return block
 
-    
     def forward(self, x):
         output = x
         for layer in self.main_block:
@@ -70,7 +70,86 @@ class ResNetBlock(nn.Module):
         output += x # add skip connection
         return output
     
+# UNET Architecture
+"""
+class DownsampleBlock(nn.Module): 
+    def __init__(self, dim : int, use_bias : bool):
+        super(DownsampleBlock, self).__init__()
+
+        self.block = self.create_block(dim, use_bias)
+    def create_block(dim : int, use_bias : bool) :
+        block = []
+        block += [nn.Conv2d(dim, dim stride=2),
+        nn.InstanceNorm2d()]
+        # Conv2d layer, stride 2
+        # padding = 1
+        # norm = instancenorm
+        # leaky relu
+        return nn.Sequential(*block)
+    
+#    def forward(self, x):
+"""
+
+class PatchDiscriminator(nn.Module):
+    """
+    Class to define a PatchGAN discriminator.
+    """
+    def __init__(self, num_channels : int,
+    num_filters : int=64, num_conv_layers : int=3, norm_layer=nn.InstanceNorm2d, ker_size : int=4, padding : int=1):
+        """
+        Constructor for PatchGAN discriminator.
+            Parameters:
+                num_channels (int) : number of channels in input (for RGB, num_channels = 3)
+                num_filters (int) : number of filters in first convolutional layer, default is 64
+                num_conv_layers (int) : number of convolutional layers, default is 3
+                norm_layer (nn.Module) : normalization layer, default is nn.InstanceNorm2d
+                ker_size (int) : kernel size to use for convolutional layers, default is 4
+                padding (int) : padding value to use, default is 1
+        """
+        super(PatchDiscriminator, self).__init__()
+        
+        # only need bias if using InstanceNorm
+        # idea taken from original CycleGAN repository
+        bias = (norm_layer == nn.InstanceNorm2d) 
+
+        block = [nn.Conv2d(num_channels, num_filters, kernel_size=ker_size, stride=2, padding=padding), 
+        nn.LeakyReLU(0.2, True)]
+
+        in_filters = num_filters
+        out_filters = None
+        for layer in range(1, num_conv_layers + 1):
+            out_filters = min(in_filters * 2, num_filters * 8) # set upper limit on size of layers
+
+            # on last layer, change stride to 1
+            if layer < num_conv_layers:
+                block.append(nn.Conv2d(in_filters, out_filters, kernel_size=ker_size, stride=2, padding=padding, bias=bias))
+            else:
+                block.append(nn.Conv2d(in_filters, out_filters, kernel_size=ker_size, stride=1, padding=padding, bias=bias))
+
+            block += [
+                norm_layer(out_filters),
+                nn.LeakyReLU(0.2, True)
+            ]
+            in_filters = out_filters
+        
+        # output layer = 1 channel
+        block.append(nn.Conv2d(out_filters, 1, kernel_size=ker_size, stride=1, padding=padding))
+        self.model = nn.Sequential(*block)
+    
+    def forward(self, x):
+        """
+        Forward method for network.
+            Parameters:
+                x (tensor) : input
+            Returns:
+                output : value after x is forwarded through the network
+        """
+        output =  self.model(x)
+        return output
 
 # test stuff
 testNet = ResNetBlock(64, dropout=False, bias=True, norm='instance')
 print(testNet)
+
+testD = PatchDiscriminator(3)
+print(testD)
